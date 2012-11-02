555316d5d1d323759cb0f788fb242b8ea982dc9d
Richard Boss <rickeboss@gmail.com>
2012-11-02 00:35:43 -0700

    Still broken, all dark, no lighting, can't fix.

--- bau5/mods/projectbench/client/TEProjectBenchRenderer.java
+++ bau5/mods/projectbench/client/TEProjectBenchRenderer.java
@@ -1,17 +1,21 @@
 package bau5.mods.projectbench.client;
 
-import org.lwjgl.opengl.GL11;
-import org.lwjgl.opengl.GL12;
 
 import cpw.mods.fml.client.FMLClientHandler;
-import static org.lwjgl.opengl.GL11.glScalef;
-import static org.lwjgl.opengl.GL11.glTranslatef;
-import static org.lwjgl.opengl.GL11.glPushMatrix;
-import static org.lwjgl.opengl.GL11.glRotatef;
-import static org.lwjgl.opengl.GL11.glEnable;
+
 import static org.lwjgl.opengl.GL11.glColor4f;
 import static org.lwjgl.opengl.GL11.glDisable;
+import static org.lwjgl.opengl.GL11.glEnable;
+import static org.lwjgl.opengl.GL11.glEndList;
+import static org.lwjgl.opengl.GL11.glGenLists;
+import static org.lwjgl.opengl.GL11.glNewList;
 import static org.lwjgl.opengl.GL11.glPopMatrix;
+import static org.lwjgl.opengl.GL11.glPushMatrix;
+import static org.lwjgl.opengl.GL11.glRotatef;
+import static org.lwjgl.opengl.GL11.glScalef;
+import static org.lwjgl.opengl.GL11.glTranslatef;
+
+import org.lwjgl.opengl.GL11;
 
 import bau5.mods.projectbench.common.ProjectBench;
 import bau5.mods.projectbench.common.TileEntityProjectBench;
@@ -35,166 +39,62 @@ public class TEProjectBenchRenderer extends TileEntitySpecialRenderer
 {
 	private RenderBlocks renderBlocks;
 
+	//Function that renders tile entity and the other block thing
 	@Override
 	public void renderTileEntityAt(TileEntity te, double x, double y,
 										 double z, float partialTick) 
 	{
+		//Null check blah blah
 		if(te == null || !(te instanceof TileEntityProjectBench))
 		{
 			return;
 		}
 		TileEntityProjectBench tpb = (TileEntityProjectBench)te;
 		renderBlocks = new RenderBlocks(tpb.worldObj);
+		//This renders the block itself
 		renderBlocks.renderBlockByRenderType(ProjectBench.instance.projectBench, 
 														 (int)x, (int)y, (int)z);
+		//Start OpenGL code
 		GL11.glPushMatrix();
-//		GL11.glDisable(2896 /*GL_LIGHTING*/);
-		renderResult(tpb, x, y, z);
-//		GL11.glEnable(2896);
-		GL11.glPopMatrix();
+		GL11.glDisable(2896 /*GL_LIGHTING*/); //disabling lighting? or something?
+		renderResult(tpb, x, y, z); //encapsulated this function cause it's recommended 
+		GL11.glEnable(2896);//enabling lighting again
+		GL11.glPopMatrix();//pop first matrix, happens last.
 	}
 	public void renderResult(TileEntityProjectBench tpb, double x, double y, double z)
 	{
+		//The result, the mini block to be rendered
 		ItemStack craftingResult = tpb.findRecipe();
 		if(craftingResult == null)
 			return;
 		GL11.glPushMatrix();
-		GL11.glTranslatef((float)x, (float)y, (float)z);
-		GL11.glEnable(32826 /*GL_RESCALE_NORMAL_EXT*/);
-		renderBlock(craftingResult, tpb, x, y, z);
+		GL11.glTranslatef((float)x, (float)y, (float)z);//Moving to block location
+		GL11.glEnable(32826 /*GL_RESCALE_NORMAL_EXT*/); //Enabling whatever this is
+		renderBlock(craftingResult, tpb, x, y, z);//encapsulated again
 		
-		GL11.glDisable(32826);
-		GL11.glPopMatrix();
+		GL11.glDisable(32826);//disable this thing
+		GL11.glPopMatrix(); //pop second matrix
 	}
 	public void renderBlock(ItemStack craftingResult, TileEntityProjectBench tpb,
 							double x, double y, double z)
 	{
+		//Moving up a bit to see
         GL11.glTranslatef(0.5F, 1.2F, 0.5F);
 		float timeD = (float) (360.0 * (double) (System.currentTimeMillis() & 0x3FFFL) / (double) 0x3FFFL);
-		glRotatef(timeD, 0.0F, 1.0F, 0.0F);
+		glRotatef(timeD, 0.0F, 1.0F, 0.0F); //spinning in circles
 
-        GL11.glScalef(0.3F, 0.3F, 0.3F);
-        this.bindTextureByName("/terrain.png");
-        GL11.glPushMatrix();
+        GL11.glScalef(0.3F, 0.3F, 0.3F); //making smaller
+        this.bindTextureByName("/terrain.png"); //texture bound
+        GL11.glPushMatrix(); //new matrix
         int var4 = (int) tpb.worldObj.getBrightness((int)x, (int)y, (int)z, tpb.worldObj.provider.lightBrightnessTable.length - 1);
        	int var5 = var4 % 65536;
         int var6 = var4 / 65536;
         OpenGlHelper.setLightmapTextureCoords(OpenGlHelper.lightmapTexUnit, (float)var5 / 1.0F, (float)var6 / 1.0F);
-       
+        //^from what i can tell does nothing
+        //v passes the block off to be rendered by the typical handler
         this.renderBlocks.renderBlockAsItem(Block.blocksList[craftingResult.itemID], 
         										craftingResult.getItemDamage(), 15);
-        GL11.glPopMatrix();
-        GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-	}
-	private void overrideTexture(Object obj) 
-	{
-		if (obj instanceof Item) 
-		{
-			GL11.glBindTexture(GL11.GL_TEXTURE_2D, FMLClientHandler.instance().getClient().renderEngine.getTexture(((Item) obj).getTextureFile()));
-		} else if (obj instanceof Block) 
-		{
-			GL11.glBindTexture(GL11.GL_TEXTURE_2D, FMLClientHandler.instance().getClient().renderEngine.getTexture(((Block) obj).getTextureFile()));
-		}
+        GL11.glPopMatrix(); //popping 3rd matrix
 	}
 }
-//	bindTextureByName("/terrain.png");
-//    this.overrideTexture(craftingResult);
-//    GL11.glPushMatrix();
-//	  GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-//	  GL11.glDisable(GL11.GL_LIGHTING);
-//	    GL11.glPushMatrix();
-//	      glTranslatef((float)x, (float)y, (float)z);
-//	      glTranslatef(0.5F, 1.2F, 0.5F);
-//	      glScalef(0.3F, 0.3F, 0.3F);
-//	        glPushMatrix();
-//              GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-//              this.renderBlocks.renderBlockAsItem(Block.blocksList[craftingResult.itemID], craftingResult.getItemDamage(), 0.5F);
-//            glPopMatrix();
-//        GL11.glPopMatrix();
-//      GL11.glDisable(GL12.GL_RESCALE_NORMAL);
-//      GL11.glEnable(GL11.GL_LIGHTING);
-//    GL11.glPopMatrix();
-	
-		//	glPushMatrix();
-		//	glColor4f(1F, 1F, 1F, 1F);
-		//	glPopMatrix();
-		//	glPushMatrix();
-		//	glDisable(GL11.GL_LIGHTING);
-		//    GL11.glEnable(GL12.GL_RESCALE_NORMAL);
-		//    GL11.glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		//	glTranslatef((float) x, (float) y, (float) z);
-		//	EntityItem customItem = new EntityItem(tileEntityRenderer.worldObj);
-		//	customItem.posX = x;
-		//	customItem.posY = y + 1;
-		//	customItem.posZ = z;
-		//	glPushMatrix();
-		//	glTranslatef(0.5F, 1.2F, 0.5F);
-		//	glScalef(0.3F, 0.3F, 0.3F);
-		//	glPushMatrix();
-		//	float rotation = (float) (360.0 * (double) (System.currentTimeMillis() & 0x3FFFL) / (double) 0x3FFFL);
-		//	
-		//	glRotatef(rotation, 0.0F, 1.0F, 0.0F);
-		//	if(craftingResult.itemID < Block.blocksList.length && Block.blocksList[craftingResult.itemID] != null
-		//			&& Block.blocksList[craftingResult.itemID].blockID != 0 
-		//			&& RenderBlocks.renderItemIn3d(Block.blocksList[craftingResult.itemID].getRenderType()))
-		//	{
-		//		bindTextureByName("/terrain.png");
-		//		overrideTexture(Block.blocksList[craftingResult.itemID]);
-		//		renderBlocks.renderBlockAsItem(Block.blocksList[craftingResult.itemID], craftingResult.getItemDamage(), 0.0F);
-		//	}
-		//	IItemRenderer customRenderer = MinecraftForgeClient.getItemRenderer(craftingResult, IItemRenderer.ItemRenderType.ENTITY);
-		//	if(customRenderer != null)
-		//	{
-		//		customItem.item = craftingResult;
-		//		bindTextureByName("/terrain.png");
-		//		overrideTexture(craftingResult.getItem());
-		//		customRenderer.renderItem(IItemRenderer.ItemRenderType.ENTITY, craftingResult, renderBlocks, customItem);
-		//	} else if(craftingResult.itemID < Block.blocksList.length && Block.blocksList[craftingResult.itemID] != null
-		//			&& Block.blocksList[craftingResult.itemID].blockID != 0 
-		//			&& RenderBlocks.renderItemIn3d(Block.blocksList[craftingResult.itemID].getRenderType()))
-		//	{
-		//		bindTextureByName("/terrain.png");
-		//		overrideTexture(Block.blocksList[craftingResult.itemID]);
-		//		renderBlocks.renderBlockAsItem(Block.blocksList[craftingResult.itemID], craftingResult.getItemDamage(), 1.0F);
-		//	} else
-		//	{
-		//		int itemTextureIndex = craftingResult.getIconIndex();
-		//		if (craftingResult.itemID >= Block.blocksList.length || Block.blocksList[craftingResult.itemID] == null || Block.blocksList[craftingResult.itemID].blockID == 0) {
-		//			bindTextureByName("/gui/items.png");
-		//			overrideTexture(Item.itemsList[craftingResult.itemID]);
-		//		} else {
-		//			bindTextureByName("/terrain.png");
-		//			overrideTexture(Block.blocksList[craftingResult.itemID]);
-		//		}
-		//		
-		//		Tessellator tessellator = Tessellator.instance;
-		//		float f5 = (float) ((itemTextureIndex % 16) * 16 + 0) / 256F;
-		//		float f8 = (float) ((itemTextureIndex % 16) * 16 + 16) / 256F;
-		//		float f10 = (float) ((itemTextureIndex / 16) * 16 + 0) / 256F;
-		//		float f12 = (float) ((itemTextureIndex / 16) * 16 + 16) / 256F;
-		//		
-		//		tessellator.startDrawingQuads();
-		//		tessellator.setNormal(0.0F, 1.0F, 0.0F);
-		//		tessellator.addVertexWithUV(0.0F - 0.5F, 0.0F - 0.25F, 0.0D, f5, f12);
-		//		tessellator.addVertexWithUV(1.0F - 0.5F, 0.0F - 0.25F, 0.0D, f8, f12);
-		//		tessellator.addVertexWithUV(1.0F - 0.5F, 1.0F - 0.25F, 0.0D, f8, f10);
-		//		tessellator.addVertexWithUV(0.0F - 0.5F, 1.0F - 0.25F, 0.0D, f5, f10);
-		//		tessellator.draw();
-		//		glScalef(-1.0F, 1.0F, 1.0F);
-		//		tessellator.startDrawingQuads();
-		//		tessellator.setNormal(0.0F, 1.0F, 0.0F);
-		//		tessellator.addVertexWithUV(0.0F - 0.5F, 0.0F - 0.25F, 0.0D, f5, f12);
-		//		tessellator.addVertexWithUV(1.0F - 0.5F, 0.0F - 0.25F, 0.0D, f8, f12);
-		//		tessellator.addVertexWithUV(1.0F - 0.5F, 1.0F - 0.25F, 0.0D, f8, f10);
-		//		tessellator.addVertexWithUV(0.0F - 0.5F, 1.0F - 0.25F, 0.0D, f5, f10);
-		//		tessellator.draw();
-		//	}
-		//	glPopMatrix();
-		//	glPopMatrix();
-		//	glPopMatrix();
-		//}
-		//
-		//glDisable(GL12.GL_RESCALE_NORMAL);
-		//glEnable(GL11.GL_LIGHTING);
-		//glColor4f(1.0F, 1.0F, 1.0F, 1.0F);
-		//glPopMatrix();
+		
