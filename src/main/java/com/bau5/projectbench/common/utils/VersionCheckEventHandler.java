1d71821b4ae341f69b4dd745bcb079d186166c66
bau5 <bau5bot@gmail.com>
2018-01-21 14:07:36 -0800

    Update VersionCheckHandler

--- src/main/java/com/bau5/projectbench/common/utils/VersionCheckEventHandler.java
+++ src/main/java/com/bau5/projectbench/common/utils/VersionCheckEventHandler.java
@@ -5,26 +5,26 @@ import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.util.text.TextComponentString;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 
 /**
  * Created by bau5 on 5/21/2015.
  */
+@Mod.EventBusSubscriber
 public class VersionCheckEventHandler{
     @SubscribeEvent
     public void playerJoinedWorld(EntityJoinWorldEvent ev){
         if(VersionChecker.didFail() || VersionChecker.isUpToDate()){
             MinecraftForge.EVENT_BUS.unregister(VersionCheckEventHandler.this);
         }else if(VersionChecker.isOutOfDate()) {
-
             if (ev.getEntity() instanceof EntityPlayer && Reference.REMOTE_VERSION != null) {
                 ChatFormatting GRAY = ChatFormatting.GRAY;
                 ChatFormatting RED = ChatFormatting.RED;
-                String message = ChatFormatting.BLUE + "Project Bench: " +GRAY +"Update available.\n";
-                message += " Version: " +RED + Reference.REMOTE_VERSION +GRAY + " Importance: " + RED +Reference.REMOTE_IMPORTANCE;
-                message += GRAY +"\n Changes: " + RED +Reference.CHANGES;
-                // ((EntityPlayer) ev.getEntity()).addChatComponentMessage(new TextComponentString(message));
-                System.out.println(message);
+                String message = ChatFormatting.BLUE + "Project Bench: " + GRAY + "Update available.\n";
+                message += " Version: " + RED + Reference.REMOTE_VERSION + GRAY + " Importance: " + RED + Reference.REMOTE_IMPORTANCE;
+                message += GRAY + "\n Changes: " + RED + Reference.CHANGES;
+                ev.getEntity().sendMessage(new TextComponentString(message));
                 MinecraftForge.EVENT_BUS.unregister(VersionCheckEventHandler.this);
             }
         }
